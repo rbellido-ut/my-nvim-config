d940655a56ff6f102680d5d9a1e579661c7819e5
Ron Bellido <1177055+rbellido-ut@users.noreply.github.com>
2023-10-01 10:41:57 -0700

    Fix missing Windows

--- lua/mappings.lua
+++
@@ -1,4 +0,0 @@
-vim.keymap.set('n', '<C-w>z', cmd 'WindowsMaximize')
-vim.keymap.set('n', '<C-w>_', cmd 'WindowsMaximizeVertically')
-vim.keymap.set('n', '<C-w>|', cmd 'WindowsMaximizeHorizontally')
-vim.keymap.set('n', '<C-w>=', cmd 'WindowsEqualize')
